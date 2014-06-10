7413f53085c47b3fff3c78826938311876cb8e03
berksarioz <bsarioz@gmail.com>
2014-06-10 16:30:43 -0400

    mapping works

--- stemmer.py
+++ stemmer.py
@@ -1,10 +1,32 @@
+# nltk helps with nlp code maintainability, but lemmatizer seems to be slow.
 from nltk.stem.wordnet import WordNetLemmatizer
-import nltk
-
+from nltk.tokenize import RegexpTokenizer
+# break user input into all lowercase words without punctuation
+tknzr = RegexpTokenizer(r'\w+')
+str = 'ipods, and caLVin klEins'
+str = str.lower()
+text = tknzr.tokenize(str)
+# get the plural words to singular form
 lmtzr = WordNetLemmatizer()
-str = 'ipods and accessories'
-
-text = nltk.word_tokenize(str)
-
 text[:] = [lmtzr.lemmatize(word) for word in text]
-print text
+
+# join the stemmed words to enable matching phrases longer than one word
+# current code does not take multiword expressions into account
+#str = ' '.join(text)
+#print str
+
+dictionary = {}
+f = open('rules.txt', 'r')
+for line in f:
+    line_arr = line.split(':')
+    val = line_arr[0]
+    keys = line_arr[1]
+    keywords = keys.split(', ')
+    #keywords = sorted(keywords, key=len) # sort according to string length
+    values = [val] * len(keywords)
+    dictionary.update(dict(zip(keywords, values)))
+
+
+
+
+print dictionary
