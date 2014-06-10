1a378c742e4f3846da04f52676ec2e32f8c75bcd
berksarioz <bsarioz@gmail.com>
2014-06-10 16:58:15 -0400

    values are printed

--- stemmer.py
+++ stemmer.py
@@ -1,12 +1,14 @@
-# nltk helps with nlp code maintainability, but lemmatizer seems to be slow.
+# This code takes a string input and outputs values according to rules.txt
+# for each word in the input.
+# NLTK helps with NLP code maintainability, but lemmatizer seems to work slow.
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
+str = 'Apple iPod 3GB'
 # break user input into all lowercase words without punctuation
-tknzr = RegexpTokenizer(r'\w+')
-str = 'ipods, and caLVin klEins'
 str = str.lower()
+tknzr = RegexpTokenizer(r'\w+')
 text = tknzr.tokenize(str)
-# get the plural words to singular form
+# get the plural words to simpler form(stemming)
 lmtzr = WordNetLemmatizer()
 text[:] = [lmtzr.lemmatize(word) for word in text]
 
@@ -15,6 +17,7 @@ text[:] = [lmtzr.lemmatize(word) for word in text]
 #str = ' '.join(text)
 #print str
 
+# map all the keywords known to their respective values from rules.txt
 dictionary = {}
 f = open('rules.txt', 'r')
 for line in f:
@@ -26,7 +29,7 @@ for line in f:
     values = [val] * len(keywords)
     dictionary.update(dict(zip(keywords, values)))
 
-
-
-
-print dictionary
+# now print all the values for each word in the input
+default = ''
+for key in text:
+	print key + ' : ' + dictionary.get(key, default)
